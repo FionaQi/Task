0754c69f3df0e1f46e9379fe96d27d95081aa53d
FionaQi <qifeng_bupt@hotmail.com>
2016-12-22 22:31:27 +0800

    Update TaskApplicationTests.java

--- src/test/java/com/pris/TaskApplicationTests.java
+++ src/test/java/com/pris/TaskApplicationTests.java
@@ -3,6 +3,7 @@ package com.pris;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
